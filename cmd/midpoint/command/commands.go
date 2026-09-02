489dc9f7ef678d412d0b6cd27d2cfa0daa10e28e
Andrea Funtò <dihedron.dev@gmail.com>
2026-09-02 23:06:14 +0200

    Fix linter directives

--- cmd/midpoint/command/commands.go
+++ cmd/midpoint/command/commands.go
@@ -9,8 +9,15 @@ import (
 
 // Commands is the set of root command groups.
 type Commands struct {
-	Roles   roles.Roles     `command:"roles" alias:"role" alias:"r" description:"Manage MidPoint roles."`
-	Users   users.Users     `command:"users" alias:"user" alias:"u" description:"Manage MidPoint users."`
-	Self    self.Read       `command:"self" alias:"s" description:"View information about oneself."`
+	// Roles manages MidPoint roles.
+	//lint:ignore SA5008 duplicate alias tags are legitimate
+	Roles roles.Roles `command:"roles" alias:"role" alias:"r" description:"Manage MidPoint roles."`
+	// Users manages MidPoint users.
+	//lint:ignore SA5008 duplicate alias tags are legitimate
+	Users users.Users `command:"users" alias:"user" alias:"u" description:"Manage MidPoint users."`
+	// Self prints information about the current user.
+	Self self.Read `command:"self" alias:"s" description:"View information about oneself."`
+	// Version prints the program version information.
+	//lint:ignore SA5008 duplicate alias tags are legitimate
 	Version version.Version `command:"version" alias:"ver" alias:"v" description:"Print program version information."`
 }
