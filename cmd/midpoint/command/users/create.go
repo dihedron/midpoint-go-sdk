87dca0b0dfad4c903337e58fac0c1f6e350d3b8b
Andrea Funtò <dihedron.dev@gmail.com>
2026-09-02 22:59:22 +0200

    Reverted handling of .env file (since MIDPOINT_DOTENV already does it automatically at init() time)

--- cmd/midpoint/command/users/create.go
+++ cmd/midpoint/command/users/create.go
@@ -10,10 +10,6 @@ import (
 )
 
 type Create struct {
-	// Name       *string        `short:"n" long:"name" description:"The username." `
-	// GivenName  *string        `short:"g" long:"name" description:"The given name of the user."`
-	// FamilyName *string        `short:"f" long:"name" description:"The family name of the user."`
-	// FullName   *string        `short:"u" long:"name" description:"The full (given + family) name of the user."`
 	User *midpoint.User `short:"u" long:"user" description:"New user's data, either as an inline value or as a @file (in JSON or YAML format)."`
 	base.Command
 }
@@ -21,29 +17,7 @@ type Create struct {
 func (cmd *Create) Execute(args []string) error {
 	slog.Debug("running user create command", "endpoint", cmd.Endpoint, "username", cmd.Username, "password", cmd.Password, "name", args)
 	mp := midpoint.New(cmd.Endpoint, cmd.Username, cmd.Password)
-
 	user := cmd.User
 	cmd.Write(os.Stdout, user)
-	// if cmd.User == nil {
-	// 	user = &midpoint.User{
-	// 		Name:      cmd.Name,
-	// 		GivenName: cmd.GivenName,
-	// 	}
-	// }
 	return mp.User.Create(context.Background(), user)
-	// self, err := mp.User.Read(context.Background(), arg)
-	// var result error
-	// for _, arg := range args {
-	// 	slog.Debug("reading user", "id", arg)
-	// 	self, err := mp.User.Read(context.Background(), arg)
-	// 	if err != nil {
-	// 		slog.Error("error reading user", "id", arg, "error", err)
-	// 		errors.Join(result, err)
-	// 		continue
-	// 	}
-	// 	if err = cmd.Write(os.Stdout, self); err != nil {
-	// 		errors.Join(result, err)
-	// 	}
-	// }
-	return nil
 }
--- internal/command/base/base.go
+++ internal/command/base/base.go
@@ -5,30 +5,16 @@ import (
 	"encoding/json/v2"
 	"fmt"
 	"io"
-	"log/slog"
 
-	"github.com/joho/godotenv"
 	"go.yaml.in/yaml/v3"
 )
 
 // Command is the base command.
 type Command struct {
-	Endpoint string  `short:"E" long:"endpoint" description:"The network endpoint for midPoint API access." required:"true" env:"MIDPOINT_ENDPOINT"`
-	Username string  `short:"U" long:"username" description:"The username for authentication." required:"true" env:"MIDPOINT_USERNAME"`
-	Password string  `short:"P" long:"password" description:"The password for authentication." required:"true" env:"MIDPOINT_PASSWORD"`
-	Format   string  `short:"F" long:"format" description:"The format of the output." optional:"true" default:"yaml" choice:"text" choice:"json" choice:"yaml" choice:"none" env:"MIDPOINT_FORMAT"`
-	DotEnv   *string `short:"D" long:"dotenv" description:"The path to the .env file." optional:"true" env:"MIDPOINT_DOTENV"`
-}
-
-func (cmd *Command) LoadEnv() error {
-	if cmd.DotEnv == nil {
-		return nil
-	}
-	if err := godotenv.Load(*cmd.DotEnv); err != nil {
-		slog.Error("error loading .env file", "error", err)
-	}
-	slog.Info("successfully loaded .env file", "path", *cmd.DotEnv)
-	return nil
+	Endpoint string `short:"E" long:"endpoint" description:"The network endpoint for midPoint API access." required:"true" env:"MIDPOINT_ENDPOINT"`
+	Username string `short:"U" long:"username" description:"The username for authentication." required:"true" env:"MIDPOINT_USERNAME"`
+	Password string `short:"P" long:"password" description:"The password for authentication." required:"true" env:"MIDPOINT_PASSWORD"`
+	Format   string `short:"F" long:"format" description:"The format of the output." optional:"true" default:"yaml" choice:"text" choice:"json" choice:"yaml" choice:"none" env:"MIDPOINT_FORMAT"`
 }
 
 func (cmd *Command) Write(stream io.Writer, object any) error {
